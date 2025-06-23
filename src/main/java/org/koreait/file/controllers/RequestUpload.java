8fd0399563be525fc911eae3a9dd89169a9e3692
songmikim <50817231+songmikim@users.noreply.github.com>
2025-06-23 21:23:55 +0900

    20250623

--- src/main/java/org/koreait/file/controllers/RequestUpload.java
+++ src/main/java/org/koreait/file/controllers/RequestUpload.java
@@ -8,4 +8,7 @@ public class RequestUpload {
     private String gid;
     private String location;
     private MultipartFile[] files;
+    private boolean single;
+    private boolean imageOnly;
+
 }
