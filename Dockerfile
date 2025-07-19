52fd031b33390d71089d6a66b7bccaa50b76a837
songmikim <50817231+songmikim@users.noreply.github.com>
2025-07-20 02:24:10 +0900

    .

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,9 @@ ENV PYTHON_BASE=/python_project/.venv/bin
 ENV PYTHON_TREND=/python_project/source
 ENV PYTHON_BASE2=/python_project/.venv/bin
 ENV PYTHON_RESTAURANT=/python_project/source
+ENV KAKAO_API_KEY=2ccabb1a6e951cdef4254a969782cb2f
+ENV DDL_AUTO=create
 
-ENTRYPOINT ["java", "-Ddb.password=${DB_PASSWORD}","-Ddb.url=${DB_URL}", "-Ddb.username=${DB_USERNAME}", "-Dfile.path=/uploads", "-Dfile.url=/uploads", "-Dpython.base=${PYTHON_BASE}", "-Dpython.trend=${PYTHON_TREND}", "-Dpython.base2=${PYTHON_BASE2}", "-Dpython.restaurant=${PYTHON_RESTAURANT}", "-Dspring.profiles.active=${SPRING_PROFILES_ACTIVE}", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-Ddb.password=${DB_PASSWORD}","-Ddb.url=${DB_URL}", "-Ddb.username=${DB_USERNAME}", "-Dfile.path=/uploads", "-Dfile.url=/uploads", "-Dpython.base=${PYTHON_BASE}", "-Dpython.trend=${PYTHON_TREND}", "-Dpython.base2=${PYTHON_BASE2}", "-Dpython.restaurant=${PYTHON_RESTAURANT}","-Dddl.auto=${DDL_AUTO}","-Dkakao.apikey=${KAKAO_API_KEY}", "-Dspring.profiles.active=${SPRING_PROFILES_ACTIVE}", "-jar", "app.jar"]
 
 EXPOSE ${PORT}
--- src/main/java/org/koreait/global/libs/Utils.java
+++ src/main/java/org/koreait/global/libs/Utils.java
@@ -174,13 +174,13 @@ public class Utils {
 
         return String.format("<img src='%s'>", url);
     }
-    /*
-    * 전체 주소 : https://site123.com:3000/member/...
-    * @param url
-    * @retrun
-    * */
-    public String getUrl(String url){
-        //String protocol = request.getScheme();  // http, https
+
+    /**
+     * 전체 주소 : https://site123.com:3000/member/....
+     * @param url
+     * @return
+     */
+    public String getUrl(String url) {
         String protocol = request.getHeader("X-Forwarded-Proto");
         protocol = StringUtils.hasText(protocol) ? protocol.split(",")[0].trim() : request.getScheme();
 
@@ -188,8 +188,6 @@ public class Utils {
         String portHeader = request.getHeader("X-Forwarded-Port");
 
         String domain = request.getServerName();
-//        int _port = request.getServerPort();
-//        String port = List.of(80, 443).contains(_port) ? "":":" + _port;
         int portNum = request.getServerPort();
 
         if (StringUtils.hasText(hostHeader)) {
@@ -209,6 +207,7 @@ public class Utils {
         }
 
         String port = List.of(80, 443).contains(portNum) ? "" : ":" + portNum;
+
         return String.format("%s://%s%s%s%s", protocol, domain, port, request.getContextPath(), url);
     }
 }
